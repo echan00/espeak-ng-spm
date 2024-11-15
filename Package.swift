895bdc2c431838d2cb5402969652ed47d746071b
Erik Chan <erik@swapbox.xyz>
2024-11-15 16:04:45 -0500

    package.swift

--- Package.swift
+++ Package.swift
@@ -3,40 +3,44 @@
 import PackageDescription
 
 let package = Package(
-    name: "espeak-ng",
-    products: [
-        .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
-        .library(name: "espeak-ng-data", targets: ["data"]),
-    ],
-    targets: [
-        .target(
-            name: "libsonic",
-            exclude: ["_repo"],
-            cSettings: []
-        ),
-        .target(
-            name: "libucd",
-            cSettings: []
-        ),
-        .target(
-            name: "data",
-            resources: [
-                .copy("espeak-ng-data"),
-                .copy("phsource"),
-                .copy("dictsource"),
-            ]
-        ),
-        .target(
-            name: "libespeak-ng",
-            dependencies: ["libsonic", "libucd"],
-            exclude: ["_repo"],
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("."),
-                .headerSearchPath("src/include"),  // Adjusted from _repo/src/include
-                .define("ESPEAK_NG_API", to: ""),
-                .define("N_PATH_HOME", to: "1024"),
-            ]
-        ),
-    ]
+  name: "espeak-ng",
+  products: [
+    .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
+    .library(name: "espeak-ng-data", targets: ["data"]),
+  ],
+  targets: [
+    .target(
+      name: "libsonic",
+      exclude: [ "_repo" ],
+      cSettings: [
+      ]
+    ),
+    .target(
+      name: "libucd",
+      cSettings: [
+      ]
+    ),
+    .target(
+      name: "data",
+      resources: [
+        .copy("espeak-ng-data"),
+        .copy("phsource"),
+        .copy("dictsource"),
+      ]
+    ),
+    .target(
+      name: "libespeak-ng",
+      dependencies: [ "libsonic", "libucd" ],
+      exclude: [
+        "_repo",
+      ],
+      publicHeadersPath: "include",
+      cSettings: [
+        .headerSearchPath("."),
+        .headerSearchPath("_repo/src/include"),
+        .define("ESPEAK_NG_API", to: ""),
+        .define("N_PATH_HOME", to: "1024"),
+      ]
+    ),
+  ]
 )
