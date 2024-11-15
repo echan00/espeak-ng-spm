52aaca2638c6c0eeefc2500854d2369578a9a6ba
Erik Chan <erik@swapbox.xyz>
2024-11-15 15:22:51 -0500

    update package.swift to point to echan00 fork of espeak-ng

--- Package.swift
+++ Package.swift
@@ -1,47 +1,58 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-  name: "espeak-ng",
-  products: [
-    .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
-    .library(name: "espeak-ng-data", targets: ["data"]),
-  ],
-  targets: [
-    .target(
-      name: "libsonic",
-      exclude: [ "_repo" ],
-      cSettings: [
-      ]
-    ),
-    .target(
-      name: "libucd",
-      cSettings: [
-      ]
-    ),
-    .target(
-      name: "data",
-      resources: [
-        .copy("espeak-ng-data"),
-        .copy("phsource"),
-        .copy("dictsource"),
-      ]
-    ),
-    .target(
-      name: "libespeak-ng",
-      dependencies: [ "libsonic", "libucd" ],
-      exclude: [
-        "_repo",
-      ],
-      publicHeadersPath: "include",
-      cSettings: [
-        .headerSearchPath("."),
-        .headerSearchPath("_repo/src/include"),
-        .define("ESPEAK_NG_API", to: ""),
-        .define("N_PATH_HOME", to: "1024"),
-      ]
-    ),
-  ]
+    name: "espeak-ng",
+    products: [
+        .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
+        .library(name: "espeak-ng-data", targets: ["data"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/echan00/espeak-ng.git", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "libsonic",
+            dependencies: [
+                .product(name: "espeak-ng", package: "espeak-ng")
+            ],
+            exclude: ["_repo"],
+            cSettings: []
+        ),
+        .target(
+            name: "libucd",
+            dependencies: [
+                .product(name: "espeak-ng", package: "espeak-ng")
+            ],
+            cSettings: []
+        ),
+        .target(
+            name: "data",
+            dependencies: [
+                .product(name: "espeak-ng", package: "espeak-ng")
+            ],
+            resources: [
+                .copy("espeak-ng-data"),
+                .copy("phsource"),
+                .copy("dictsource"),
+            ]
+        ),
+        .target(
+            name: "libespeak-ng",
+            dependencies: [
+                .product(name: "espeak-ng", package: "espeak-ng"),
+                "libsonic",
+                "libucd"
+            ],
+            exclude: ["_repo"],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("_repo/src/include"),
+                .define("ESPEAK_NG_API", to: ""),
+                .define("N_PATH_HOME", to: "1024"),
+            ]
+        ),
+    ]
 )
